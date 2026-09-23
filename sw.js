e90ca0464223201c97c435488b061982b4cae57c
Sathya <100759989+sathyanarayanan1309@users.noreply.github.com>
2026-09-23 10:13:27 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,17 @@
+var CACHE = 'msc-v1';
+self.addEventListener('install', function(e) { self.skipWaiting(); });
+self.addEventListener('activate', function(e) {
+  e.waitUntil(caches.keys().then(function(keys) {
+    return Promise.all(keys.filter(function(k){ return k !== CACHE; }).map(function(k){ return caches.delete(k); }));
+  }));
+});
+self.addEventListener('fetch', function(e) {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    fetch(e.request).then(function(r) {
+      var rc = r.clone();
+      caches.open(CACHE).then(function(c){ c.put(e.request, rc); });
+      return r;
+    }).catch(function(){ return caches.match(e.request); })
+  );
+});
